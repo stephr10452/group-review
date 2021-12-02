93c3467ba960c9b146a41f13c2bef3830d3f1771
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-12-02 17:00:24 -0500

    Added About comp

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+
+
+function About() {
+    return ( 
+        <>
+          <h1>About the group</h1>
+          <div>Steph</div>
+          <div>Josh</div>
+          <div>Cherry</div>
+          <div>Mk</div>
+        </>
+     );
+}
+
+export default About ;
